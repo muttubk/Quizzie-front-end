e873b7b9f7a19e97dbc7c7819a8f94bf71a93deb
muttu <muttubk140601@gmail.com>
2024-01-29 17:27:31 +0530

    added login/signup validations

--- src/components/LoginSignup/LoginSignup.jsx
+++ src/components/LoginSignup/LoginSignup.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './LoginSignup.module.css'
+
+import { useNavigate } from 'react-router-dom'
 
 function Signup(props) {
     const [formData, setFormData] = useState({
@@ -8,12 +10,14 @@ function Signup(props) {
         password: '',
         confirmPassword: ''
     })
-    const [error, setError] = useState({
-        name: '',
-        email: '',
-        password: '',
-        confirmPassword: ''
-    })
+
+    const [error, setError] = useState({})
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        setError({})
+    }, [props])
+
     const handleInputChange = (e) => {
         const { name, value } = e.target
         setFormData({
@@ -26,68 +30,85 @@ function Signup(props) {
         })
     }
 
-    // const validateName = () => {
-    //     if (!formData.name) {
-    //         setError((prev) => ({ ...prev, name: "Name is required" }))
-    //     }
-    //     else {
-    //         setError((prev) => ({ ...prev, name: '' }))
-    //     }
-    // }
-    // const validateEmail = () => {
-    //     if (!formData.email) {
-    //         setError((prev) => ({ ...prev, email: "Email is required" }))
-    //     }
-    //     else if (!(/\S+@\S+\.\S+/.test(formData.email))) {
-    //         setError((prev) => ({ ...prev, email: 'Invalid email' }))
-    //     }
-    //     else {
-    //         setError((prev) => ({ ...prev, email: '' }))
-    //     }
-    // }
-    // const validatePassword = () => {
-    //     if (!formData.password) {
-    //         setError((prev) => ({ ...prev, password: "Password is required" }))
-    //     }
-    //     else {
-    //         setError((prev) => ({ ...prev, password: '' }))
-    //     }
-    // }
-    // const validateConfirmPassword = () => {
-    //     if (!formData.confirmPassword) {
-    //         setError((prev) => ({ ...prev, confirmPassword: "Re-enter password" }))
-    //     }
-    //     else if (formData.confirmPassword !== formData.password) {
-    //         setFormData({
-    //             ...formData,
-    //             confirmPassword: ''
-    //         })
-    //         setError((prev) => ({ ...prev, confirmPassword: "Password doesn't match" }))
-    //     }
-    //     else {
-    //         setError((prev) => ({ ...prev, confirmPassword: '' }))
-    //     }
-    // }
-
-    const handleSignup = (e) => {
-        e.preventDefault()
-        // validateName()
-        // validateEmail()
-        // validatePassword()
-        // validateConfirmPassword()
-
-        // if (Object.keys(error).every(item => !error[item])) {
-        //     console.log(formData)
-        // }
-        // else {
-        //     console.log("Invalid form data")
-        // }
-        console.log("handle sign up")
+    // Object for storing validating functions for each field
+    const validate = {
+        name: (() => {
+            if (formData.name.length === 0) {
+                return 'Name is required'
+            } else {
+                return ''
+            }
+        })(),
+        email: (() => {
+            if (formData.email.length === 0) {
+                return 'Email is required'
+            } else if (!(/\S+@\S+\.\S+/.test(formData.email))) {
+                return 'Enter valid email address'
+            } else {
+                return ''
+            }
+        })(),
+        password: (() => {
+            if (formData.password.length === 0) {
+                return 'Password is required'
+            } else if (formData.password.length < 6 && props.type === 'signup') {
+                return 'Minimum password length is 6'
+            } else {
+                return ''
+            }
+        })(),
+        confirmPassword: (() => {
+            if (formData.confirmPassword.length === 0) {
+                return 'Re-enter password'
+            } else if (formData.password !== formData.confirmPassword) {
+                return "Passwords doesn't match"
+            } else {
+                return ''
+            }
+        })()
     }
+
+    // To validate given fields
+    const validateFields = (fields) => {
+        let newErrors = {}
+        fields.forEach(field => {
+            newErrors[field] = validate[field]
+        })
+        setError(newErrors)
+    }
+
+    // handle signup
+    const handleSignup = (e) => {
+        e.preventDefault();
+        const fields = ["name", "email", "password", "confirmPassword"]
+        validateFields(fields);
+        let isValid = fields.every(field => error[field] === '')
+        if (isValid) {
+            console.log(formData)
+            navigate('/dashboard')
+        } else {
+            console.log(error)
+        }
+    }
+
+    // For real-time validation of corresponding field
+    // const handleBlur = (e) => {
+    //     const { name } = e.target
+    //     setErrors({
+    //         ...errors, [name]: validate[name]
+    //     })
+    // }
 
     const handleLogin = (e) => {
         e.preventDefault()
-        console.log("handle login")
+        const fields = ["email", "password"]
+        validateFields(fields)
+        let isValid = fields.every(field => error[field] === '')
+        if (isValid) {
+            console.log(formData.email, formData.password)
+        } else {
+            console.log(error)
+        }
     }
 
     return (
@@ -100,9 +121,13 @@ function Signup(props) {
                         <input type="text" name="name" id="name"
                             value={formData.name}
                             onChange={handleInputChange}
-                            placeholder={error.name}
+                            // placeholder="Enter name"
                             style={{ border: error?.name && "1.5px solid #D60000" }}
                         />
+                        {
+                            error.name &&
+                            <small className={styles.errorMessage}>{error.name}</small>
+                        }
                     </div>
                 }
                 <div>
@@ -110,18 +135,26 @@ function Signup(props) {
                     <input type="email" name="email" id="email"
                         value={formData.email}
                         onChange={handleInputChange}
-                        placeholder={error.email}
+                        // placeholder="Enter email"
                         style={{ border: error?.email && "1.5px solid #D60000" }}
                     />
+                    {
+                        error.email &&
+                        <small className={styles.errorMessage}>{error.email}</small>
+                    }
                 </div>
                 <div>
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password"
                         value={formData.password}
                         onChange={handleInputChange}
-                        placeholder={error.password}
+                        // placeholder="Enter password"
                         style={{ border: error?.password && "1.5px solid #D60000" }}
                     />
+                    {
+                        error.password &&
+                        <small className={styles.errorMessage}>{error.password}</small>
+                    }
                 </div>
                 {
                     props.type === 'signup' &&
@@ -130,9 +163,13 @@ function Signup(props) {
                         <input type="password" name="confirmPassword" id="confirmPassword"
                             value={formData.confirmPassword}
                             onChange={handleInputChange}
-                            placeholder={error.confirmPassword}
+                            // placeholder="Re-enter password"
                             style={{ border: error?.confirmPassword && "1.5px solid #D60000" }}
                         />
+                        {
+                            error.confirmPassword &&
+                            <small className={styles.errorMessage}>{error.confirmPassword}</small>
+                        }
                     </div>
                 }
                 {
